018e86311e02b65a14d2d6f8ae03c87c4ebc160d
L3lis <132632980+L3lis@users.noreply.github.com>
2025-10-17 18:52:01 -0300

    Update RepositorioServico.java

--- TrabalhoApsPooML/Trabalho4/src/dados/RepositorioServico.java
+++ TrabalhoApsPooML/Trabalho4/src/dados/RepositorioServico.java
@@ -1,5 +1,48 @@
 package dados;
+import java.util.ArrayList;
+import java.util.List;
+import model.Servico;
 
 public class RepositorioServico {
+    protected List<Servico> servicos;
+    protected int quantSer;
+
+    public RepositorioServico(){
+
+    }
+
+    public void Add(Servico s){
+        contarServico();
+        if (s != null){
+            servicos.add(s);
+        }
+    }
+
+    public void Excluir(int id){
+        contarServico();
+        for (int i = 0; i < quantSer; i++) {
+            if (servicos.get(i).getId() == id)
+            servicos.remove(servicos.get(i));
+        }
+    }
+
+    public void Alterar(int id){
+        contarServico();
+        for (int i = 0; i < quantSer; i++) {
+            if (servicos.get(i).getId() == id){
+                
+            }
+        }
+    }
+
+    protected void contarServico() {
+		quantSer = 0;
+		for (int i = 0; i < servicos.size(); i++) {
+			if (servicos.get(i) != null) {
+				quantSer++;
+			}
+		}
+	}
 
 }
+
