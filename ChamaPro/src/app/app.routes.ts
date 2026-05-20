a021b547e7d9750cdb9c28c8e069afe80f7d405f
GabrielMiranda8 <gabrielnascimentobiel16@gmail.com>
2026-05-19 21:54:42 -0300

    Interface Login 1

--- ChamaPro/src/app/app.routes.ts
+++ ChamaPro/src/app/app.routes.ts
@@ -2,16 +2,16 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full',
   },
   {
     path: 'cadastro',
     loadComponent: () => import('./pages/cadastro/cadastro.page').then( m => m.CadastroPage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
   },
 ];
