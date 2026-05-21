484187c41bcee2f251804f96f3640612a2f7a954
Lelis <fofinhopisando@gmail.com>
2026-05-20 21:45:20 -0300

    Add login, menu, view, and update routes

--- ChamaPro/src/app/app.routes.ts
+++ ChamaPro/src/app/app.routes.ts
@@ -7,11 +7,23 @@ export const routes: Routes = [
     pathMatch: 'full',
   },
   {
-    path: 'cadastro',
-    loadComponent: () => import('./pages/cadastro/cadastro.page').then( m => m.CadastroPage)
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then((m) => m.LoginPage),
   },
   {
-    path: 'login',
-    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+    path: 'cadastro',
+    loadComponent: () => import('./pages/cadastro/cadastro.page').then((m) => m.CadastroPage),
+  },
+  {
+    path: 'menu',
+    loadComponent: () => import('./pages/menu/menu.page').then((m) => m.MenuPage),
+  },
+  {
+    path: 'view',
+    loadComponent: () => import('./pages/view/view.page').then((m) => m.ViewPage),
+  },
+  {
+    path: 'update',
+    loadComponent: () => import('./pages/update/update.page').then((m) => m.UpdatePage),
   },
 ];
