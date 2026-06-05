63d418afbc1ac08eaa64f509188a5cbda4f87453
Lelis <fofinhopisando@gmail.com>
2026-06-05 15:15:32 -0300

    ts pedidos

--- ChamaPro/src/app/tabs/pedidos/pedidos.page.ts
+++ ChamaPro/src/app/tabs/pedidos/pedidos.page.ts
@@ -1,20 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+
+import {
+  IonContent,
+  IonIcon
+} from '@ionic/angular/standalone';
+
+import { addIcons } from 'ionicons';
+import {
+  timeOutline,
+  checkmarkCircleOutline,
+  constructOutline
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-pedidos',
   templateUrl: './pedidos.page.html',
   styleUrls: ['./pedidos.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    CommonModule,
+    IonContent,
+    IonIcon
+  ]
 })
-export class PedidosPage implements OnInit {
+export class PedidosPage {
 
-  constructor() { }
+  pedidos = [
+    {
+      servico: 'Reparo elétrico',
+      profissional: 'Ana Silva',
+      valor: 160,
+      data: 'Hoje, 14:00',
+      status: 'andamento'
+    },
+    {
+      servico: 'Manutenção do Sistema',
+      profissional: 'Carlos Santos',
+      valor: 140,
+      data: 'Amanhã, 09:00',
+      status: 'agendado'
+    },
+    {
+      servico: 'Reparar Cerca Elétrica',
+      profissional: 'Maria Oliveira',
+      valor: 480,
+      data: '22 Fev, 10:00',
+      status: 'concluido'
+    },
+    {
+      servico: 'Colocar Painel',
+      profissional: 'Pedro Lima',
+      valor: 2000,
+      data: '18 Fev, 15:00',
+      status: 'concluido'
+    }
+  ];
 
-  ngOnInit() {
+  constructor() {
+    addIcons({
+      'time-outline': timeOutline,
+      'checkmark-circle-outline': checkmarkCircleOutline,
+      'construct-outline': constructOutline
+    });
   }
 
 }
