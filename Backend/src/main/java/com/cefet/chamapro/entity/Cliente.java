0a51fc830e4356266041e834eab3fb170edd43d5
Lelis <fofinhopisando@gmail.com>
2026-06-17 20:27:15 -0300

    Cliente Usuario Entity

--- Backend/src/main/java/com/cefet/chamapro/entity/Cliente.java
+++ Backend/src/main/java/com/cefet/chamapro/entity/Cliente.java
@@ -1,5 +1,17 @@
 package com.cefet.chamapro.entity;
 
-public class Cliente {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Table(name = "tb_cliente")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cliente extends Usuario {
 }
--- Backend/src/main/java/com/cefet/chamapro/entity/Usuario.java
+++ Backend/src/main/java/com/cefet/chamapro/entity/Usuario.java
@@ -1,5 +1,49 @@
 package com.cefet.chamapro.entity;
 
-public class Usuario {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Table(name = "tb_usuario")
+@Inheritance(strategy = InheritanceType.JOINED)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 200, unique = true)
+    private String nome;
+
+    @Column(nullable = false, length = 200, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 200)
+    private String senha;
+
+    @Column(nullable = false, length = 200, unique = true)
+    private String cpf;
+
+    @Column(nullable = false, length = 200)
+    private String dtNasc;
+
+    @Column(nullable = false, length = 200)
+    private String dtConta;
+
+    @Column(nullable = false)
+    private Number nota;
+
+    @Column(nullable = false, length = 200)
+    private String tipo;
 }
