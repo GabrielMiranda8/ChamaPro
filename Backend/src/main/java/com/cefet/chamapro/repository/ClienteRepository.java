5bb467258cd1f6b363d345eacf3a4257192ca72a
Lelis <fofinhopisando@gmail.com>
2026-06-17 20:28:13 -0300

    Cliente Usuario Repository

---
+++ Backend/src/main/java/com/cefet/chamapro/repository/ClienteRepository.java
@@ -0,0 +1,7 @@
+package com.cefet.chamapro.repository;
+
+import com.cefet.chamapro.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+}
---
+++ Backend/src/main/java/com/cefet/chamapro/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package com.cefet.chamapro.repository;
+
+import com.cefet.chamapro.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+    Optional<Usuario> findByCpf(String cpf);
+    boolean existsByEmail(String email);
+    boolean existsByCpf(String cpf);
+}
