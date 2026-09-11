bebd53e007c983bde36d8302e6747248958a3272
GabrielMiranda8 <gabrielnascimentobiel16@gmail.com>
2026-09-10 21:37:54 -0300

    BACK - Correcao Security Cors

--- Backend/src/main/java/com/cefet/chamapro/config/SecurityConfig.java
+++ Backend/src/main/java/com/cefet/chamapro/config/SecurityConfig.java
@@ -60,7 +60,6 @@ public class SecurityConfig {
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("Authorization"));
-        config.setAllowCredentials(false);
         config.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
